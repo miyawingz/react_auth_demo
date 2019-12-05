347027279bea72e7ab820bbc37c9dd35dad25c01
Mia Zhang <miyawingz@gmail.com>
2019-12-04 20:57:23 -0800

    setup basic page layout

--- src/components/app.js
+++ src/components/app.js
@@ -4,12 +4,22 @@ import 'materialize-css/dist/js/materialize.min';
 import '../assets/css/app.scss';
 import { Route } from 'react-router-dom';
 import Home from './home';
+import AboutUs from './about_us';
+import Nav from './nav';
+import SecretList from './secret_list';
+import SignIn from './sign_in';
 import SignUp from './sign_up';
 
 const App = () => (
-    <div className="container">
-        <Route exact path="/" component={Home} />
-        <Route path="/sign-up" component={SignUp} />
+    <div>
+        <Nav />
+        <div className="container">
+            <Route exact path="/" component={Home} />
+            <Route path="/about-us" component={AboutUs} />
+            <Route path="/secret-list" component={SecretList} />
+            <Route path="/sign-in" component={SignIn} />
+            <Route path="/sign-up" component={SignUp} />
+        </div>
     </div>
 );
 
